b27aa27d229b3e5b0eb717e1f0284f4b89552c3e
Ghadi <grt04@mail.aub.edu>
2021-10-11 18:16:43 +0300

    Fixed UI and Play Again button bug.

--- script.js
+++ script.js
@@ -9,51 +9,61 @@ const buttons = document.querySelectorAll("button")
 
 function playRound (playerSelection, computerSelection) {
    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-       return (`It's a tie, you both played ${playerSelection.toLowerCase()}`)
+       return (`It's a tie, you both played ${playerSelection}`)
 
    } else if (playerSelection.toLowerCase() == "rock") {
        if (computerSelection.toLowerCase() == "paper") {
            computerScore += 1;
-           return (`You Lose! Paper beats Rock.`)
+           return (`You played <span class="blue">${playerSelection}</span>, Computer played <span class="red">${computerSelection}</span>.<br>
+           You Lose! Paper beats Rock.`)
            
        } else if (computerSelection.toLowerCase() == "scissors"){
             playerScore += 1;   
-            return (`You Win! Rock beats Scissors.`)
+            return (`You played <span class="blue">${playerSelection}</span>, Computer played <span class="red">${computerSelection}</span>.<br> 
+            You Win! Rock beats Scissors.`)
        }
    } else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection.toLowerCase() == "scissors") {
             computerScore += 1;
-            return (`You Lose! Scissors beats Paper.`)
+            return (`You played <span class="blue">${playerSelection}</span>, Computer played <span class="red">${computerSelection}</span>.<br>
+            You Lose! Scissors beats Paper.`)
         } else if (computerSelection.toLowerCase() == "rock"){
             playerScore += 1; 
-            return (`You Win! Paper beats Rock.`)
+            return (`You played <span class="blue">${playerSelection}</span>, Computer played <span class="red">${computerSelection}</span>.<br>
+            You Win! Paper beats Rock.`)
         }
     } else if (playerSelection.toLowerCase() == "scissors") {
     if (computerSelection.toLowerCase() == "paper") {
         playerScore += 1; 
-        return (`You Win! Scissors beats Paper.`)
+        return (`You played <span class="blue">${playerSelection}</span>, Computer played <span class="red">${computerSelection}</span>.<br>
+        You Win! Scissors beats Paper.`)
     } else if (computerSelection.toLowerCase() == "rock"){
         computerScore += 1;
-        return (`You Lose! Rock beats Scissors.`)
+        return (`You played <span class="blue">${playerSelection}</span>, Computer played <span class="red">${computerSelection}</span>.<br>
+        You Lose! Rock beats Scissors.`)
 }}}
 function disableButtons() {
     buttons.forEach(button => {
+        if(button.value == "reset"){
+            button.disabled = false;
+        }else{
         button.disabled = true;
-    })
+    }})
 }
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         if(button.value == "reset"){
             location.reload();
         }else{
-            console.log("button clicked")
-            document.getElementById("result").innerText = playRound(button.value, computerPlay());
-            document.getElementById("scoreboard").innerText = `Player: ${playerScore} -- Computer: ${computerScore}`;
+            document.getElementById("result").innerHTML = playRound(button.value, computerPlay());
+            document.getElementById("scoreboard").innerHTML = `<span class="blue">Player: ${playerScore}</span> -- <span class="red">Computer: ${computerScore}</span>`;
             if (playerScore == 5) {
-                document.getElementById("final-result").innerText= "Congrats! You win!";
+                document.getElementById("final-result").classList.add("win");
+                document.getElementById("final-result").innerHTML= "Congrats! You win!";
                 disableButtons();
             } else if (computerScore == 5) {
-                document.getElementById("final-result").innerText= "Oh no! You lost!";
+                document.getElementById("final-result").classList.add("lose");
+                document.getElementById("final-result").innerHTML= "Oh no! You lost!";
                 disableButtons();
                 }
             }})
